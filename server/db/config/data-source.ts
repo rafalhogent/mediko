d4c18c5bd2e0119b84d8538962a75a48e8140f9b
rafalhogent <rafal.kowalski@student.hogent.be>
2025-05-27 08:38:14 +0200

    AB#121: init db, install typeorm, mysql2, migration scripts

---
+++ server/db/config/data-source.ts
@@ -0,0 +1,19 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+dotenv.configDotenv();
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'mariadb',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? '3000'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [process.env.ENTITIES_PATH ?? "dist/**/*.entity.js"],
+  migrations: ['dist/db/migrations/*.js'],
+  logging: true
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
